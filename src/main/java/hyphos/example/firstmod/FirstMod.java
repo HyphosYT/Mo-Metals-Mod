c24fe7e61b357bc2ec2d3872d4c3b35a7644f690
HyphosYT <92472193+HyphosYT@users.noreply.github.com>
2022-03-15 11:17:11 -0400

    Removed Spinach

--- src/main/java/hyphos/example/firstmod/FirstMod.java
+++ src/main/java/hyphos/example/firstmod/FirstMod.java
@@ -15,8 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FirstMod implements ModInitializer {
-
-	public static final Item SPINICH = new Item(new Item.Settings().group(ItemGroup.FOOD));
 	
 	public static final Block TITANIUM_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL));
 	
@@ -28,7 +26,6 @@ public class FirstMod implements ModInitializer {
 	public void onInitialize() {
 	
 	Registry.register(Registry.BLOCK, new Identifier("firstmod", "titanium_block"), TITANIUM_BLOCK);
-	Registry.register(Registry.ITEM, new Identifier("firstmod", "spinach"), SPINICH);
 	Registry.register(Registry.ITEM, new Identifier("firstmod", "titanium_block"), new BlockItem(TITANIUM_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 	Registry.register(Registry.ITEM, new Identifier("firstmod", "titanium"), TITANIUM);
 
