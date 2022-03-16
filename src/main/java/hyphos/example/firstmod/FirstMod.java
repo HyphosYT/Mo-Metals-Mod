89af258af39d1e95553856aaa0c999fa801c2e72
HyphosYT <92472193+HyphosYT@users.noreply.github.com>
2022-03-16 08:24:28 -0400

    Added Titanium Ore but drops currently not working

--- src/main/java/hyphos/example/firstmod/FirstMod.java
+++ src/main/java/hyphos/example/firstmod/FirstMod.java
@@ -3,6 +3,7 @@ package hyphos.example.firstmod;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -20,6 +21,8 @@ public class FirstMod implements ModInitializer {
 	
 	public static final Item TITANIUM = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
 
+	public static final Block TITANIUM_ORE = new TitaniumBlock(FabricBlockSettings.copy(Blocks.STONE));
+
 	public static final Logger LOGGER = LoggerFactory.getLogger("firstmod");
 
 	@Override
@@ -28,6 +31,8 @@ public class FirstMod implements ModInitializer {
 	Registry.register(Registry.BLOCK, new Identifier("firstmod", "titanium_block"), TITANIUM_BLOCK);
 	Registry.register(Registry.ITEM, new Identifier("firstmod", "titanium_block"), new BlockItem(TITANIUM_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 	Registry.register(Registry.ITEM, new Identifier("firstmod", "titanium"), TITANIUM);
+	Registry.register(Registry.BLOCK, new Identifier("firstmod", "titanium_ore"), TITANIUM_ORE);
+	Registry.register(Registry.ITEM, new Identifier("firstmod", "titanium_ore"), new BlockItem(TITANIUM_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 
 		LOGGER.info("Hello Fabric world!");
 	}
